045b20813b5d1058b22a890e8a0167f13f2f017e
AtikAlgur <96806151+AtikAlgur@users.noreply.github.com>
2022-03-31 16:35:36 +0000

    distance

--- p1original.c
+++ p1original.c
@@ -1,2 +1,26 @@
-
+#include <stdio.h>
+#include<math.h>
+void input(float *x1,float *y1,float *x2,float *y2)
+{
+    printf("Enter the values of x1,y1 and x2,y2\n");
+    scanf("%f %f %f %f",x1,y1,x2,y2);
+}
+void find_distance( float x1, float y1, float x2, float y2, float *area)
+{
+    float x=x2-x1;
+    float y=y2-y1;
+    *area=sqrt(pow(x*x,2)+pow(y*y,2));
+}
+void output(float x1, float y1,float x2, float y2, float area)
+{
+    printf("The area is %f",&area);
+}
+int main()
+{
+    float x1,y1,x2,y2,area;
+    input (&x1,&y1,&x2,&y2);
+    
+    find_distance( x1, y1, x2, y2, &area);
+    output( x1,  y1, x2,  y2,  area);
+    return 0;
 }
